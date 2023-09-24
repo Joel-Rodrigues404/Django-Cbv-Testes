ef591a61bef3288a96d8b2c602b052bc688279a6
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-09-24 18:53:51 -0300

    teste 015 agora vai merdaa

--- CBV_django_test/settings.py
+++ CBV_django_test/settings.py
@@ -141,9 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-# MEDIA_URLS ='/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
