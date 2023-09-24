e2eda07826750a3ba2e4f43cc39f0544d4384b08
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-09-24 18:13:57 -0300

    teste 03

--- CBV_django_test/settings.py
+++ CBV_django_test/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDITAR_ROOT = os.path.join(BASE_DIR, 'media')
