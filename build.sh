87544df525b936cd07367a9c0e4a518ebbc41bc1
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-09-25 20:59:04 -0300

    teste 034

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
  python3 manage.py makemigrations --noinput
  python3 manage.py migrate --noinput
  echo "COLLECTSTATIC"
- python3 manage.py collectstatic --noinput --clear
+ python3 manage.py collectstatic --noinput
  echo "BUILD END"
