68f79ac9238fa59c1bf53242179b16576c8ec161
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-09-25 21:18:19 -0300

    teste 039

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
- echo "BUILD START"
- echo "INSTALL REQUIREMENTS"
- python3.9 -m pip install -r requirements.txt
- echo "MAKEMIGRATIONS"
- python3.9 manage.py makemigrations --noinput
- python3.9 manage.py migrate --noinput
- echo "COLLECTSTATIC"
- python3.9 manage.py collectstatic --noinput
- echo "BUILD END"
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
